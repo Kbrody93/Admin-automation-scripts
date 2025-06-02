a0bcae92cdd1ed8c1d5491a6e3be6bfa36f949d7
Kbrody93 <mrbmailme@proton.me>
2025-06-02 11:11:03 -0500

    Create adminprofile.zsh
    initial commit

---
+++ adminprofile.zsh
@@ -0,0 +1,21 @@
+USERNAME="********"  
+FULLNAME="********"
+PASSWORD="********"
+
+echo "Checking if user '$USERNAME' exists..."
+
+if id "$USERNAME" &>/dev/null; then
+    echo "User '$USERNAME' exists. Removing..."
+    sudo sysadminctl -deleteUser "$USERNAME" -secure
+    sleep 2
+fi
+
+echo "Creating admin user '$USERNAME'..."
+sudo sysadminctl -addUser "$USERNAME" -fullName "$FULLNAME" -password "$PASSWORD" -admin
+
+if id "$USERNAME" &>/dev/null; then
+    echo "User '$USERNAME' created successfully and added as admin."
+else
+    echo "Failed to create user '$USERNAME'."
+    exit 1
+fi
